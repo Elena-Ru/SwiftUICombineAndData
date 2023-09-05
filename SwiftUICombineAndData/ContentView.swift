2d8df8e61c1cb25c224bd85c3ddfe44df6e12461
Elena <lenarusskih@gmail.com>
2023-09-05 15:07:49 +0300

    Add TrackableScrollView

--- SwiftUICombineAndData/ContentView.swift
+++ SwiftUICombineAndData/ContentView.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  @State private var contentOffset = CGFloat(0)
+  var body: some View {
+    NavigationView {
+      ZStack(alignment: .top) {
+        Color.primaryBackground
+          .edgesIgnoringSafeArea(.all)
+        TrackableScrollView { offset in
+          contentOffset = offset.y
+        } content: {
+          Text("Hello, world!")
+            .foregroundColor(.white)
+            .padding()
         }
-        .padding()
+        Color.secondaryBackground
+          .opacity(contentOffset < -16 ? 1 : 0)
+          .animation(.easeIn)
+          .ignoresSafeArea()
+          .frame(height: 0)
+      }
+      .frame(maxHeight: .infinity, alignment: .top)
+      .navigationBarHidden(true)
     }
+    .navigationViewStyle(StackNavigationViewStyle())
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ SwiftUICombineAndData/Extensions/UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor.swift
+//  SwiftUICombineAndData
+//
+//  Created by Елена Русских on 2023-09-05.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  static let primaryBackground = Color("primaryBackground")
+  static let secondaryBackground = Color("secondaryBackground")
+}
